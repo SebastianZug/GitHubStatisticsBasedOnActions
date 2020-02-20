6d5e21947d2e1f659d086a711a72acd309d894b9
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2020-02-20 18:14:53 +0100

    new version of python scripts

--- statistics/generateStatistic.py
+++ statistics/generateStatistic.py
@@ -1,136 +1,90 @@
-from datetime import date, timedelta, datetime
+from datetime import datetime
 import subprocess
-import io
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import pathlib
 from pytz import reference
 
-
-def days(start, upto):
-    td = upto - start
-    day_count = int(td/timedelta(days=1))
-    return [start + timedelta(days=x) for x in range(day_count)]
+relevant_file_types = ['.md']
 
 
-def git_get_first_commit():
-    command = ['git', 'log', '--reverse', '--date=short']
-    result = subprocess.run(command,
-                            check=True,
-                            stdout=subprocess.PIPE,
-                            universal_newlines=True).stdout
-    filtered = io.StringIO(result.replace('\n\n', '\n'))
-    for line in filtered:
-        if 'Date' in line:
-            date_str = line.replace('\n', '').split(' ')[-1]
-            date_ = [int(i) for i in date_str.split('-')]
-            break
-    return date(date_[0], date_[1], date_[2])
+def git_log_history_command():
+    return ['git', 'log', '--numstat',
+            '--pretty=format:"%x09%x09%x09%h%x09%at%x09%aN"']
 
 
-def git_log_command(date):
-    end = date + timedelta(days=1)
-    return ['git', 'log',
-            '--after={0} 00:00'.format(date.isoformat()),
-            '--before={0} 00:00'.format(end.isoformat()),
-            '--format=format:',
-            '--shortstat',
-            'master',
-            '--', '*.md']
+def git_log_projectName_command():
+    return ['git', 'remote', '-v']
 
 
-def get_key_values(string_array, substring):
-    index = [idx for idx, s in enumerate(string_array) if substring in s]
-    if len(index):
-        return index[0]
-    else:
-        return -1
+def run_git_command(command):
+    return subprocess.run(command, check=True,
+                          stdout=subprocess.PIPE,
+                          universal_newlines=True).stdout
 
 
-def git_get_projectname():
-    command = ['git', 'remote', '-v']
-    result = subprocess.run(command,
-                            check=True,
-                            stdout=subprocess.PIPE,
-                            universal_newlines=True).stdout
-    result = result.replace('\t', '\n')
-    return result.split('\n')[1]
+def get_project_name():
+    project_info = run_git_command(git_log_projectName_command())
+    return project_info.replace('\t', ' ').split(' ')[1]
 
 
-def generate_data(start, upto):
-    activities_per_day = []
-    interval = days(start, upto)
-    print("Reading time interval of " + str(len(interval)) + " days ...")
-    for d in interval:
-        daily_stat = {}
-        result = subprocess.run(git_log_command(d),
-                                check=True,
-                                stdout=subprocess.PIPE,
-                                universal_newlines=True).stdout
-        filtered = io.StringIO(result.replace('\n\n', '\n'))
-        commits = 0
-        lines = 0
-        for line in filtered:
-            content = line.split(' ')
-            index = get_key_values(content, 'file')
-            if index != -1:
-                commits = commits + int(content[index-1])
-            index = get_key_values(content, 'insertion')
-            if index != -1:
-                lines = lines + int(content[index-1])
-            index = get_key_values(content, 'deletion')
-            if index != -1:
-                lines = lines - int(content[index-1])
-        daily_stat["day"] = d
-        daily_stat["commits"] = commits
-        daily_stat["lines"] = lines
-        activities_per_day.append(daily_stat)
-
-    df = pd.DataFrame(activities_per_day)
-    if len(activities_per_day):
-        df['day'] = pd.to_datetime(df['day'])
-        df.set_index('day', inplace=True)
-        #df.drop(df[df.commits == 0].index, inplace=True)
-        print("... {0} activity days with {1} lines of code found.".format(df.lines.astype(bool).sum(axis=0), df.lines.sum()))
-
-    return df
+def get_history_data(filename='git.log'):
+    git_hist = run_git_command(git_log_history_command())
+    git_hist = git_hist.replace('"', ' ')
+    with open(filename, 'w') as fp:
+        fp.write(git_hist)
+    git_log = pd.read_csv(filename, sep="\t", header=None,
+                            names=['additions', 'deletions', 'filename',
+                                'sha', 'timestamp', 'author'])
+    git_log = git_log[['additions', 'deletions', 'filename']]\
+            .join(git_log[['sha', 'timestamp', 'author']]\
+            .fillna(method='ffill'))\
+            .dropna()
+    git_log['timestamp'] = pd.to_datetime(git_log.timestamp, unit='s')
+    git_log.set_index('timestamp', inplace=True)
+    git_log['extention'] = git_log.filename.apply(lambda path: pathlib.PurePosixPath(path).suffix) 
+    git_log.loc[git_log['additions'] == '-', 'additions'] = np.nan
+    git_log.loc[git_log['deletions'] == '-', 'deletions'] = np.nan
+    git_log['line_count'] = git_log.additions.astype(float) -\
+                            git_log.deletions.astype(float)
+    git_log.loc[:, 'commit_count'] = 1
+    git_log['commit_count'] = git_log['commit_count'].rolling(git_log.shape[0]).count().astype('int')
+    print("Found {0} commits in project's history".format(git_log.sha.unique().shape[0]))
+    return git_log
 
 
 def generate_diagram(project_name, data, interval, filename):
-    df = data.groupby(pd.Grouper(freq=interval)).sum()
-    df['lines_sum'] = df.lines.cumsum().astype('int64')
-    df.drop(['lines'], axis=1, inplace=True)
-    print(df.head())
-    fig, ax = plt.subplots()
-    df.lines_sum.plot(drawstyle="steps-mid", linewidth=2, ax=ax)
-    ax.set_title(project_name)
-    df.plot(secondary_y=['commits'], drawstyle="steps-mid", ax=ax)
-    ax.set_xlabel(filename)
-    ax.set_ylabel('Lines of Code')
-    ax.right_ax.set_ylabel('Commits per ' + filename)
-    plt.tight_layout()
-    #plt.show()
-    fig.savefig(filename+".png")
-    print("File saved to " + filename + ".png")
+    df = data[["line_count", "commit_count"]].resample(interval).agg({"line_count": "sum", "commit_count": "max"})
+    df['line_count_cumsum']= df.line_count.cumsum().astype('int')
+    df.drop(["line_count"], axis=1, inplace=True)
+    if df.shape[0] > 1:
+        fig, ax = plt.subplots()
+        df.line_count_cumsum.plot(drawstyle="steps-mid", linewidth=2, ax=ax)
+        ax.set_title(project_name)
+        df.plot(secondary_y=['commit_count'], drawstyle="steps-mid", ax=ax)
+        ax.set_xlabel(filename)
+        ax.set_ylabel('Lines of Code')
+        ax.right_ax.set_ylabel('Commits per ' + filename)
+        plt.tight_layout()
+        plt.show()
+        fig.savefig(filename+".png")
+        print("File saved to " + filename + ".png")
+    else:
+        print("Not enought data for printing `" + interval + "` diagram")
 
 
 if __name__ == "__main__":
     localtime = reference.LocalTimezone()
     print("Time zone of the server: " + str(localtime.tzname(datetime.now())))
-    project_name = git_get_projectname()
+    project_name = get_project_name()
     print("Evaluating project " + project_name)
-    start = git_get_first_commit()
-    data = generate_data(start - timedelta(days=1),
-                         date.today() + timedelta(days=7))
+    data = get_history_data()
+    filtered = data[data.extention.isin(relevant_file_types)]
     intervals = {
         "Day": 'D',
         "Week": "W",
         "Month": 'M',
         "Year": 'Y'}
     for name, abbrevation in intervals.items():
-        generate_diagram(project_name, data, abbrevation, name)
-
-    # Example for individual filter
-    # Visualize code generation during last 30 days
-    data = generate_data(date.today() - timedelta(days=30),
-                         date.today() + timedelta(days=1))
-    generate_diagram(project_name, data, 'D', "LastMonth")
+        generate_diagram(project_name, filtered, abbrevation, name)
